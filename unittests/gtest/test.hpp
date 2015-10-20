5ff14b8d3ebf0a6b434e4eca45b3cf1b842b5146
Guillaume Marcais <gmarcais@umd.edu>
2015-10-20 18:08:10 -0400

    Added jellyfish headers. Remove dependency on Jellyfish. Unittesting for wholesequenceparser.

--- unittests/gtest/test.hpp
+++ unittests/gtest/test.hpp
@@ -1,7 +1,25 @@
 #ifndef __TEST_H__
 #define __TEST_H__
 
+#include <unistd.h>
+
 #include <random>
+
+// Random generator initialized with the correct seed.
 extern std::mt19937 rand_gen;
+
+
+// RAII. Automatically unlink a file when object goes out of scope.
+struct file_unlink {
+  std::string path;
+  bool do_unlink;
+  explicit file_unlink(const char* s, bool d = true) : path(s), do_unlink(d) { }
+  explicit file_unlink(const std::string& s, bool d = true) : path(s), do_unlink(d) { }
+  ~file_unlink() {
+    if(do_unlink)
+      unlink(path.c_str());
+  }
+};
+
 
 #endif /* __TEST_H__ */
---
+++ unittests/test_cooperative_pool2.cc
@@ -0,0 +1,95 @@
+#include <thread>
+#include <vector>
+
+#include <gtest/gtest.h>
+#include <jellyfish/cooperative_pool2.hpp>
+
+namespace {
+// Generate all numbers in [0, producers * max)
+class sequence : public jellyfish::cooperative_pool2<sequence, int> {
+  typedef jellyfish::cooperative_pool2<sequence, int> super;
+
+  const uint32_t        max_;
+  std::vector<uint32_t> cur_;
+  const uint32_t        producers_;
+
+public:
+  sequence(uint32_t producers, uint32_t threads, uint32_t max)
+    : super(producers, 4 * threads)
+    , max_(max)
+    , cur_(producers, 0)
+    , producers_(producers)
+  { }
+
+  bool produce(uint32_t i, int& e) {
+    assert(i < producers_);
+    auto& cur = cur_[i];
+    if(cur < max_) {
+      e = i * max_ + cur++;
+      return false;
+    }
+    return true;
+  }
+};
+
+class CooperativePoolTest : public ::testing::TestWithParam<uint32_t> {
+public:
+  static const uint32_t max_ = 10000;
+  std::vector<unsigned char> check_;
+  unsigned int nb_threads_;
+  sequence seq_;  // Generator of ints
+
+  CooperativePoolTest()
+    : check_(GetParam() * max_, 0)
+    , nb_threads_(std::thread::hardware_concurrency())
+    , seq_(GetParam(), nb_threads_, max_)
+
+  { }
+
+  bool check() const {
+    for(const auto it : check_)
+      if(it != 1)
+        return false;
+    return true;
+  }
+
+  bool check_print() const {
+    if(check())
+      return true;
+
+    for(int it : check_)
+      std::cout << it;
+    std::cout << "\n----------\n";
+    // for(auto it = seq_.check_.cbegin(); it != seq_.check_.cend(); ++it)
+    //   std::cout << (int)*it;
+    std::cout << "\n";
+    return false;
+  }
+
+  //protected:
+};
+
+void thread_work(sequence* const seq, std::vector<unsigned char>* const check) {
+  while(true) {
+    sequence::job j(*seq);
+    if(j.is_empty())
+      break;
+    ++(*check)[*j];
+  }
+}
+
+TEST_P(CooperativePoolTest, Ints) {
+  std::vector<std::thread> threads; // Thread handles
+
+  for(unsigned i = 0; i < nb_threads_; ++i)
+    threads.push_back(std::thread(thread_work, &seq_, &check_));
+  for(auto& th : threads)
+    th.join();
+  EXPECT_TRUE(check_print());
+}
+
+INSTANTIATE_TEST_CASE_P(CooperativePool,
+                        CooperativePoolTest,
+                        ::testing::Range((uint32_t)1, (uint32_t)5));
+
+} // namespace {
