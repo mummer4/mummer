2c8faacced3d5c36674ee6d321944efe15f866b7
Guillaume Marcais <gmarcais@umd.edu>
2015-12-02 11:01:36 -0500

    Check automake version.

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -8,3 +8,8 @@ if [ ! -e "$HOME/bin/yaggo" ]; then
     curl -L -o ~/bin/yaggo https://github.com/gmarcais/yaggo/releases/download/v1.5.9/yaggo
     chmod a+rx ~/bin/yaggo
 fi
+
+# Check automake version
+automake --version
+automake --version | \
+    ruby -ne '$_ =~ /(\d+)\.(\d+)\.(\d+)/ and ($1.to_i > 1 || ($1.to_i == 1 && $2.to_i > 13)) and exit(0); puts("Automake is too old"); exit(1)'
