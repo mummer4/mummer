93c4b85316fa9e567664df82959e1c0b3b845556
Guillaume Marcais <gmarcais@umd.edu>
2015-12-02 11:18:07 -0500

    Install autoconf 2.69

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -19,12 +19,27 @@ if [ ! -e "$HOME/bin/automake" ]; then
     make install
 fi
 
+# Install autoconf 2.69
+if [ ! -e "$HOME/bin/autoconf" ]; then
+    wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
+    tar zxf autoconf-2.69.tar.gz
+    cd autoconf-2.69
+    ./configure --prefix="$HOME"
+    make
+    make install
+fi
+
 # Check exec
 echo "PATH: $PATH"
 echo -n "yaggo: "; which yaggo
 echo -n "automake: "; which automake
+echo -n "autoconf: "; which autoconf
 
-# Check automake version (Should not fail!)
+# Check automake && autoconf version (Should not fail!)
 automake --version
 automake --version | \
     ruby -ne '$_ =~ /(\d+)\.(\d+)\.(\d+)/ and ($1.to_i > 1 || ($1.to_i == 1 && $2.to_i > 13)) and exit(0); puts("Automake is too old"); exit(1)'
+
+autoconf --version
+autoconf --version | \
+    ruby -ne '$_ =~ /(\d+)\.(\d+)/ and ($1.to_i > 2 || ($1.to_i == 2 && $2.to_i > 68)) and exit(0); puts("Autoconf is too old"); exit(1)'
