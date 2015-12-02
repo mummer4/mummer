851780e0917a3ae78deda9afd19482aa16ee5d7f
Guillaume Marcais <gmarcais@umd.edu>
2015-12-02 11:11:35 -0500

    Install automake 1.14

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -9,7 +9,22 @@ if [ ! -e "$HOME/bin/yaggo" ]; then
     chmod a+rx ~/bin/yaggo
 fi
 
-# Check automake version
+# Install automake 1.14
+if [ ! -e "$HOME/bin/automake" ]; then
+    wget ftp://ftp.gnu.org/gnu/automake/automake-1.14.1.tar.gz
+    tar zxf automake-1.14.1.tar.gz
+    cd automake-1.14.1
+    ./configure --prefix="$HOME"
+    make
+    make install
+fi
+
+# Check exec
+echo "PATH: $PATH"
+echo -n "yaggo: "; which yaggo
+echo -n "automake: "; which automake
+
+# Check automake version (Should not fail!)
 automake --version
 automake --version | \
     ruby -ne '$_ =~ /(\d+)\.(\d+)\.(\d+)/ and ($1.to_i > 1 || ($1.to_i == 1 && $2.to_i > 13)) and exit(0); puts("Automake is too old"); exit(1)'
