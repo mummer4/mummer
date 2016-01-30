7d4db9d706ff45bf64dc42220dfdb5f3030637ea
Guillaume Marcais <gmarcais@umd.edu>
2016-01-29 19:33:39 -0500

    New switch -L,--minalign to filter out small alignments.

--- tests/nucmer.sh
+++ tests/nucmer.sh
@@ -3,3 +3,6 @@ nucmer --mum --delta /dev/stdout $D/seed_reads_1.fa $D/seed_reads_0.fa | \
 
 nucmer --maxmatch --delta /dev/stdout $D/seed_reads_1.fa $D/seed_reads_0.fa | \
     ufasta sort -H | test_md5 d782ac6370f1d285828292ecbe8d7705
+
+nucmer --maxmatch --delta /dev/stdout -L 90 $D/seed_reads_1.fa $D/seed_reads_0.fa | \
+    ufasta sort -H | test_md5 7f0dfdaf740f1a1889a28c0735d0ec46
